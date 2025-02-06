9360f364327b9dc0edaa3a05863f64df78a007c6
VamCart <vamcart@gmail.com>
2025-02-06 17:18:53 +0300

    Добавлена admin страница

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -1,10 +1,9 @@
-const AuthLayout = ({children}: React.ReactNode) => {
+const AuthLayout = ({ children }: React.ReactNode) => {
   return (
-    <div className="h-full flex items-center justify-center flex h-full flex-col items-center 
-    justify-center bg-[radial-gradient(ellipse_at_top,var(--tw-gradient-stops))] from-sky-400 to-blue-800">
+    <div className="h-full flex flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,var(--tw-gradient-stops))] from-sky-400 to-blue-800">
       {children}
     </div>
-  )
-}
+  );
+};
 
 export default AuthLayout;
